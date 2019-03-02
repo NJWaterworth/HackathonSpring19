d759ad468b99d9feb05ed876ef8d7e846bcd7c63
Nick W <n.waterworth@verizon.net>
2019-03-02 10:13:33 -0600

    Updated audio Detection

--- AudioTest.js
+++ AudioTest.js
@@ -1,26 +1,53 @@
 var five = require("johnny-five");
 var board = new five.Board();
 
+//Global variables 
 var data = [];
-var bodyCount = -1;
-var cases = 6;
-var Threshold = 50;
-var BaseCase = 600;
+var bodyCount = 0;
 
+//Meta-Parameters
+var cases = 10;
+var Threshold = 50;
+var OuterCase = 200;
+var InnerCase = 50;
+
+//Function to check if someone is there 
 function checkPerson(){
-	
 	var sum = 0;
 	
+	//Check to see if Cases have been acquired
 	if(data.length > cases){
+		
+		//Get previous cases
 		for(let i = 0; i < data.length - 2; i++){
 			sum = sum + data[i];
-		}
-		if(sum/(cases - 1) - data[data.length - 2] > Threshold){
-			bodyCount = bodyCount + 1;
-			console.log("New Person Found! Total People:" + bodyCount);
+		}		
+		
+		//Calculate Delta
+		var change = (data[data.length - 2] - data[0]) %1;		
+		
+		//Check if change is past threshold
+		if(sum/(cases - 1) - data[data.length - 2] > Threshold && data[data.length - 2] < OuterCase){
+			//Add to count if they are moving towards
+			if(change < 0 && InnerCase < data[data.length - 2]){
+				bodyCount = bodyCount + 1;
+				console.log("New Person Found! Total People: " + bodyCount);
+			}
+			//else delete
+			else if(InnerCase > data[data.length - 2]){
+				if(bodyCount > 0){
+					bodyCount = bodyCount - 1;
+					console.log("Person Left! Total People: " + bodyCount);
+				}
+			}
+			//Get rid of extraneous data
 			data.splice(0,data.length - 2);
 		}
-		else
+		
+		/*else if(sum/(cases - 1) - data[data.length - 2] > Threshold && data[data.length - 2] < OuterCase){
+			console.log("LEaving");
+		}*/
+		else //shift data by one 
 			data.shift();
 	}
 }
@@ -36,10 +63,5 @@ board.on("ready", function() {
   proximity.on("change", function() {
 	data.push(this.cm);
 	checkPerson();
-	
-	/*
-	data.forEach(function(item, index, array){
-		console.log("Number: " + item + " index: " +index);
-		});*/
 	});
 });
